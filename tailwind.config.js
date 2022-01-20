9fe9fcc718b0e6f2400df07edeac96db74a5a292
Aayush jain <aayushjain1118@gmail.com>
2022-01-21 02:23:53 +0530

    semi-final

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
 	content: ['*.{html,js}'],
 	theme: {
-		extend: {},
+		extend: {
+			gridTemplateColumns: {
+				'1-4': '1fr 4fr',
+			},
+		},
+		plugins: [],
 	},
-	plugins: [],
 };
